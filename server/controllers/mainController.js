f7d9a5e633d3f0ab7e6ea35c2d795048a82137f5
AD\jnguyen <jnguyen1@gaikai.com>
2018-07-26 14:58:02 -0700

    Messages

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -7,7 +7,7 @@ const newsapi = new NewsApi('e565a5235f6643608297fad059d2d3d3');
 
 mongoose.connect('mongodb://dragonking99:7!28CcsZRZpy@ds253871.mlab.com:53871/psn-watch')
 
-// Create a schema
+// Create schemas
 const userSchema = new mongoose.Schema({
     userId: Number,
 	name: String,
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
 	avatar: String,
 	level: Number,
 	levelupPercent: Number,
-	language: String,
+    language: String,
+    followers: Number,
 	trophies: [{
 		game: {
 			title: String,
@@ -30,19 +31,69 @@ const userSchema = new mongoose.Schema({
 	}]
 });
 
-const User = mongoose.model('User', userSchema)
+const messageSchema = new mongoose.Schema({
+    from: Number,
+    to: Number,
+    message: String,
+    timestamp: Date
+});
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    timestamp: Number,
+    user: {
+        userId: Number,
+        name: String,
+        username: String,
+        online: Boolean,
+        game: String,
+        avatar: String,
+        level: Number,
+        levelupPercent: Number,
+        language: String,
+        followers: Number,
+        trophies: [{
+            game: {
+                title: String,
+                image: String
+            },
+            bronze: Number,
+            silver: Number,
+            gold: Number,
+            completion: Number
+        }]
+    },
+    likes: Number,
+    images: [String]
+});
+
+const User = mongoose.model('User', userSchema);
+const Message = mongoose.model('Message', messageSchema);
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = (app) => {
-    app.get('/messages', (req, res) =>{
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    })); 
 
+    app.get('/messages/:to/:from', (req, res) => {
+        Message.find({$or:[{to: req.params.to, from: req.params.from}, {to: req.params.from, from: req.params.to}]}).sort('-date').exec((err, messages) => {
+            res.json(messages);
+        })
     });
 
     app.post('/messages', (req, res) => {
-
+        var message = new Message({to: req.body.to, from:req.body.from, message: req.body.message, date: Date.now()})
+        message.save((err, message) => {
+            if(err) {
+                throw err;
+            }
+            res.status(200).json({messageId: message._id})
+        })
     });
     
     app.get('/friends', (req, res) => {
-        console.log('hit');
         User.find({}).where('userId').ne(1).exec((err, user) => {
             if(err) {
                 throw err;
@@ -98,13 +149,28 @@ module.exports = (app) => {
     });
 
     app.get('/posts', (req, res) => {
-        var posts = {
-
-        }
+        Post.find({}, (err, posts) => {
+            if(err) {
+                throw err;
+            }
+            res.json(posts)
+        })
     });
 
     app.post('/posts', (req, res) => {
-
+        User.findOne({userId: req.body.userId}, (err, currentUser) => {
+            if (err) {
+                throw err;
+            }
+            console.log(currentUser);
+            var post = new Post({title: req.body.title, timestamp: Date.now(), user: currentUser, likes: 0, images: req.body.images})
+            post.save((err, post) => {
+                if(err) {
+                    throw err;
+                }
+                res.status(200).json({postId: post._id})
+            })
+        });
     });
 
     app.get('/profile', (req, res) => {;   
